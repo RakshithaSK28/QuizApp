92849bf24b83291d5f8c51a8f9eaea9f2c15749f
RakshithaSK28 <rakshithask28sk@gmail.com>
2025-01-06 12:25:05 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        Question: 'What is redux?',
+        Option1: 'Redux is state management system',
+        Option2: 'Redux is state',
+        Option3: 'Redux is data type',
+        Option4: 'Redux is a part of HTML'
+    },
+    {
+        Question: 'What is the primary use of redux?',
+        Option1: 'To manage and centralize atspplication state',
+        Option2: 'To create CSS styles',
+        Option3: 'To handle API reques',
+        Option4:  'To manage HTML templates'
+    },
+    {
+        Question: 'What are the core principles of Redux?',
+        Option1: 'Single source of truth, state is read-only, changes are made with pure',
+        Option2: 'State is shared, changes are made with impure functions',
+        Option3: 'State is distributed, changes are automatic',
+        Option4: 'None of the above'
+    }, 
+    {
+        Question: 'Which method is used to send data to the Redux store',
+        Option1: 'dispatch()',
+        Option2: 'send()',
+        Option3: 'update()',
+        Option4: 'setState()'
+    },
+    {
+        Question: 'What is an action in redux?',
+        Option1: 'A plain JavaScript object describing what happend',
+        Option2: 'A function to update to DOM',
+        Option3: 'A method to fetch API data',
+        Option4: 'A way to handle user input'
+    }
+];
